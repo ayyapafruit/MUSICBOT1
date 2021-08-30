bb01c6df7b8139e6dc3f114c7a830f233ec3e378
ayyapafruit <89799873+ayyapafruit@users.noreply.github.com>
2021-08-30 22:37:34 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -11,7 +11,7 @@ async def start(_, message: Message):
     await message.reply_text(
         f"""**Hey, I'm {bn} 🎵
 
-I can play music in your group's voice call. Developed by [Jason](https://t.me/ImJanindu).
+I can play music in your group's voice call. Developed by [DONVIP](https://t.me/sensibleguy).
 
 Add me to your group and play music freely!**
         """,
@@ -22,10 +22,10 @@ Add me to your group and play music freely!**
                         "🛠 Source Code 🛠", url="https://github.com/Infinity-Bots/GroupMusicPlayerBot")
                   ],[
                     InlineKeyboardButton(
-                        "💬 Group", url="https://t.me/InfinityBOTs_Support"
+                        "💬 Group", url="https://t.me/English_Hindi_chattingg"
                     ),
                     InlineKeyboardButton(
-                        "🔊 Channel", url="https://t.me/Infinity_BOTs"
+                        "🔊 Channel", url="https://t.me/English_Hindi_chattingg"
                     )
                 ],[ 
                     InlineKeyboardButton(
