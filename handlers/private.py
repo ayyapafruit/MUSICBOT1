a4a5188741d9e3252ceaacdbcfdd01dee46756f3
ayyapafruit <89799873+ayyapafruit@users.noreply.github.com>
2021-08-31 20:10:49 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -7,7 +7,6 @@ from helpers.filters import other_filters2
 
 @Client.on_message(other_filters2)
 async def start(_, message: Message):
-    await message.reply_sticker("CAACAgQAAx0CTv65QgABBfJlYF6VCrGMm6OJ23AxHmD6qUSWESsAAhoQAAKm8XEeD5nrjz5IJFYeBA")
     await message.reply_text(
         f"""**Hey, I'm {bn} 🎵
 
@@ -17,9 +16,7 @@ Add me to your group and play music freely!**
         """,
         reply_markup=InlineKeyboardMarkup(
             [
-                [
-                    InlineKeyboardButton(
-                        "🛠 Source Code 🛠", url="https://github.com/Infinity-Bots/GroupMusicPlayerBot")
+               
                   ],[
                     InlineKeyboardButton(
                         "💬 Group", url="https://t.me/English_Hindi_chattingg"
